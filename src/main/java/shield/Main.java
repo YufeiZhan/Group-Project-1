eff19fa1aa8b8bfff748b158b99f6b283a0a2918
YifanXuJ <s1923493@ed.ac.uk>
2021-04-23 14:41:41 +0800

    Save Main

--- src/main/java/shield/Main.java
+++ src/main/java/shield/Main.java
@@ -5,14 +5,50 @@
 
 package shield;
 
+import java.time.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("running!");
-//    DummyShieldingIndividualClientImp individual =  new DummyShieldingIndividualClientImp("http://0.0.0.0:5000/");
-//    Collection<String> boxes = individual.showFoodBoxes("vegan");
-//    System.out.println(boxes);
+    
+    //之前register过的 重新new instance之后，就没办法retrieve info
+    //所以isregisered 却没有办法通过server reset user的info
+    
+    //ShieldingIndividualClientImp test = new ShieldingIndividualClientImp("http://0.0.0.0:5000/");
+    //test.registerShieldingIndividual("1212120144");
+    //assert test.isRegistered();
+    
+    
+    //test.getClosestCateringCompany();
+    //test.pickFoodBox(1);
+    //boolean check = test.placeOrder();
+    //System.out.println(check);
+    
+  
+  
+  
+    //Collection<String> boxes = test.showFoodBoxes("none");
+   // Collection<String> l = test.getCateringCompanies();
+    
+    //for(String e: l){
+      //System.out.println(e);
+    //}
+    //System.out.println("e");
+    //String a = "EH1_9RG";
+    //System.out.println(a.matches("EH([1-9]|(1[0-7]))_[1-9][A-Z][A-Z]"));
+    //test.registerShieldingIndividual("1212120103");
+    
+//  DummyShieldingIndividualClientImp individual =  new DummyShieldingIndividualClientImp("http://0.0.0.0:5000/");
+//  Collection<String> boxes = individual.showFoodBoxes("vegan");
+//  System.out.println(boxes);
+    
     return;
   }
 }
